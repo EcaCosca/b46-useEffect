7f2e9dbaf039d43dd89434b180e846a386f9bd27
EcaCosca <enriquecoscarelli@gmail.com>
2023-12-14 10:57:04 +0100

    DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+  const [visible, setVisible] = useState(true);
+  const [text, setText] = useState("Hello");
+
+  // useEffect(()=>{
+  //   console.log('useEffect is working')
+  // },[todos])
+
+  // https://www.codementor.io/@damianpereira/how-to-use-clearinterval-inside-react-s-useeffect-and-why-it-is-important-1si7mztjlk
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      console.log(`Current blinking text: ${text}`);
+      setVisible((visible) => !visible);
+      setCount(count => count + 1);
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [text]);
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>useEffect</h1>
+      <h3>{count}</h3>
+      <input type="text" onChange={(e)=>setText(e.target.value)}/>
+      {visible ? <h1>{text}</h1> : null}
+      {/* <h3>{count}</h3>
+      <button onClick={()=>setCount(count+1)}>+</button>
+      <br/>
+      <h4>To Dos</h4>
+      <input type="text" onChange={(e)=>setTodos([...todos, e.target.value])}/>
+      <ul>
+        {todos.map((todo, index)=>{
+          return <li key={index}>{todo}</li>
+        })}
+      </ul> */}
     </>
   )
 }
